d3eed8095022ab1ccd64732e792422ae74d1295c
Binh Thanh Bui <64269565+JackAtlas10@users.noreply.github.com>
2022-07-05 00:43:32 +0700

    add element-wise multiplication cos sine

--- utils.cpp
+++ utils.cpp
@@ -113,11 +113,13 @@ void resp::circshift(Mat &src, Mat &dst, Size shiftSize)
     Mat tempMat = Mat::zeros(src.size(), src.type());
     int nrow = src.rows;
     int ncol = src.cols;
-    
+
     int nrow_s = shiftSize.height % nrow;
-    if (nrow_s < 0) nrow_s += nrow;
+    if (nrow_s < 0)
+        nrow_s += nrow;
     int ncol_s = shiftSize.width % ncol;
-    if( ncol_s < 0) ncol_s += ncol;
+    if (ncol_s < 0)
+        ncol_s += ncol;
     // shift rows
     Rect roi_in(Point(0, nrow - nrow_s), Point(ncol, nrow));
     Rect roi_out(Point(0, 0), Point(ncol, nrow_s));
@@ -135,27 +137,142 @@ void resp::circshift(Mat &src, Mat &dst, Size shiftSize)
     tempMat(roi_in).copyTo(dst(roi_out));
 }
 
+void resp::shift_sample(Mat &inOutMat, Size shift, Mat kx, Mat ky)
+{
+    /*
+    Shift a sample in the Fourier domain. The shift should be normalized to
+    the range [-pi, pi].
+    */
+    assert(kx.cols == 1 && ky.rows == 1);
+    kx.convertTo(kx, CV_32FC1);
+    kx = kx * shift.width;
+    ky.convertTo(ky, CV_32FC1);
+    ky = ky * shift.height;
+
+    Mat shift_exp_x(kx.size(), CV_32FC2);
+    Mat shift_exp_y(ky.size(), CV_32FC2);
+    vector<Mat> x_planes;
+    split(shift_exp_x, x_planes);
+    vector<Mat> y_planes;
+    split(shift_exp_y, y_planes);
+};
+
 void MyMat::make_arr(Mat &inOutArr, int a, int b)
 {
-    assert(a!=b);
+    assert(a != b);
     int factor = 1;
-    int num = b-a+1;
-    if(a>b)
+    int num = b - a + 1;
+    if (a > b)
     {
         factor = -1;
-        num = - num;
+        num = -num;
     }
-    if(a==b)
+    if (a == b)
         num = 1;
     inOutArr = Mat::zeros(Size(1, num), CV_32FC1);
     float start = a;
-    for(int i=0; i<num; ++i)
+    for (int i = 0; i < num; ++i)
     {
-        inOutArr.at<float>(i) = start + i*factor;
+        inOutArr.at<float>(i) = start + i * factor;
     }
 }
 
-bool updateRefMu(Mat &response_diff,  float &refMu, float zeta, float nu)
+Mat MyMat::e_mul(const Mat &a, const Mat &b)
+{
+    /*
+    b must be vector-like mat object or b has size equal to a;
+    */
+    int a_row = a.rows, a_col = a.cols, a_chan = a.channels();
+    int b_row = b.rows, b_col = b.cols, b_chan = b.channels();
+    assert(b_row == 1 || b_col == 1 || (b_row == a_row && b_col == a_col));
+    assert(b_row != 1 || b_col != 1);
+    assert(b_chan <= a_chan);
+    Mat ans;
+    if (b_row == 1)
+    {
+        Mat temp = Mat::zeros(a.size(), a.type());
+        vector<Mat> planes;
+        split(temp, planes);
+        for (int chanId = 0; chanId < a_chan; ++chanId)
+        {
+            Rect roi;
+            for (int i = 0; i < a_row; ++i)
+            {
+                roi = Rect(Point(0, i), Point(a_col, i + 1));
+                b.copyTo(planes[chanId](roi));
+            }
+        }
+        merge(planes, temp);
+        return a.mul(temp);
+    }
+    else if (b_col == 1)
+    {
+        Mat temp = Mat::zeros(a.size(), a.type());
+        vector<Mat> planes;
+        split(temp, planes);
+        for (int chanId = 0; chanId < a_chan; ++chanId)
+        {
+            Rect roi;
+            for (int i = 0; i < a_col; ++i)
+            {
+                roi = Rect(Point(i, 0), Point(i + 1, a_row));
+
+                b.copyTo(planes[chanId](roi));
+            }
+        }
+        merge(planes, temp);
+        return a.mul(temp);
+    }
+    else
+    {
+        Mat temp = Mat::zeros(a.size(), a.type());
+        vector<Mat> planes;
+        split(temp, planes);
+        for (int chanId = 0; chanId < a_chan; ++chanId)
+        {
+            Rect roi = Rect(Point(0, 0), Point(a_row, a_col));
+            b.copyTo(planes[chanId](roi));
+        }
+        merge(planes, temp);
+        return a.mul(temp);
+    }
+}
+
+Mat MyMat::e_cos(const Mat &inImg)
+{
+    assert(inImg.channels() == 1);
+    int rows = inImg.rows;
+    int cols = inImg.cols;
+    Mat ans = Mat::zeros(inImg.size(), CV_32FC1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            ans.at<float>(i, j) = cos(inImg.at<float>(i, j));
+        }
+    }
+    return ans;
+}
+
+Mat MyMat::e_sin(const Mat &inImg)
+{
+    assert(inImg.channels() == 1);
+    int rows = inImg.rows;
+    int cols = inImg.cols;
+    Mat ans = Mat::zeros(inImg.size(), CV_32FC1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            ans.at<float>(i, j) = sin(inImg.at<float>(i, j));
+        }
+    }
+    return ans;
+}
+
+bool updateRefMu(Mat &response_diff, float &refMu, float zeta, float nu)
 {
     bool occ;
     response_diff.convertTo(response_diff, CV_32FC1);
@@ -165,10 +282,10 @@ bool updateRefMu(Mat &response_diff,  float &refMu, float zeta, float nu)
     Mat w, u, vt;
     svd.compute(response_diff, w, u, vt, SVD::NO_UV);
     cv::MatIterator_<float> max_addr = std::max_element(w.begin<float>(), w.end<float>());
-    float eta = *max_addr/10000.0f;
+    float eta = *max_addr / 10000.0f;
     if (eta < phi)
     {
-        refMu = m / (1 + log(nu*eta+1));
+        refMu = m / (1 + log(nu * eta + 1));
         return false;
     }
     else
@@ -177,4 +294,3 @@ bool updateRefMu(Mat &response_diff,  float &refMu, float zeta, float nu)
         return true;
     }
 }
-
--- utils.h
+++ utils.h
@@ -1,29 +1,37 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/opencv_modules.hpp>
 #include <iostream>
-#include <cmath>
-using namespace cv;
+// #include <math.h>
 using namespace std;
+using namespace cv;
 
 namespace fft
 {
-    void calculateDFT2(Mat &src, Mat &dst);
+    void calculateDFT2(cv::Mat &src, cv::Mat &dst);
 
-    void resizedDft2(Mat &complexImg, Mat &resizedDftOutput, Size desiredSize);
+    void resizedDft2(cv::Mat &complexImg, cv::Mat &resizedDftOutput, Size desiredSize);
 
-    void calculateIDFT2(Mat &src, Mat &dst);
+    void calculateIDFT2(cv::Mat &src, cv::Mat &dst);
 }
 
 namespace resp
 {
-    void circshift( Mat& src, Mat& dst, Size shift_size);
+    void circshift( cv::Mat& src, cv::Mat& dst, Size shift_size);
+
+    void shift_sample(cv::Mat &inOutMat, Size shift, cv::Mat kx, cv::Mat ky);
 
     void respDiff();
 }
 
 namespace MyMat 
 {
-    void make_arr(Mat &inOutArr, int a, int b);
+    void make_arr(cv::Mat &inOutArr, int a, int b);
+
+    cv::Mat e_mul(const cv::Mat&a, const cv::Mat&b);
+
+    Mat e_cos(const Mat &in);
+
+    Mat e_sin(const Mat &in);
 }
 
-bool updateRefMu(Mat &response_diff, float &refMu, float zeta=13.0f, float nu=0.3f);
+bool updateRefMu(cv::Mat &response_diff, float &refMu, float zeta=13.0f, float nu=0.3f);
